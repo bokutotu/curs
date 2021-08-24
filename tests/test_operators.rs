4ead6c6dc8c799e10103376825258ff4caa2adfc
bokutotu <mushin.hudoushin@gmail.com>
2021-08-25 01:20:42 +0900

    update

--- tests/test_operators.rs
+++ tests/test_operators.rs
@@ -1,4 +1,3 @@
-
 use curs::array::Array;
 use curs::CursState;
 
@@ -15,15 +14,8 @@ fn test_add() {
         source_vec_b.push(i as f32 * 2.);
         ans.push(i as f32 * 3.);
     }
-    let source_array_a = Array::from_vec(
-        source_vec_a, 
-        &vec![10, 100], 
-        &status).unwrap();
-    let source_array_b = Array::from_vec(
-        source_vec_b, 
-        &vec![10, 100], 
-        &status).unwrap();
-
+    let source_array_a = Array::from_vec(source_vec_a, &vec![10, 100], &status).unwrap();
+    let source_array_b = Array::from_vec(source_vec_b, &vec![10, 100], &status).unwrap();
 
     let res = source_array_a + source_array_b;
 
@@ -34,24 +26,18 @@ fn test_add() {
 fn test_sub() {
     let status = CursState::new(0);
 
-    let mut source_vec_a = Vec::<f32>::with_capacity(1000);
-    let mut source_vec_b = Vec::<f32>::with_capacity(1000);
-    let mut ans = Vec::<f32>::with_capacity(1000);
+    let mut source_vec_a = Vec::<f32>::with_capacity(100);
+    let mut source_vec_b = Vec::<f32>::with_capacity(100);
+    let mut ans = Vec::<f32>::with_capacity(100);
 
-    for i in 0..1000 {
+    for i in 0..100 {
         source_vec_a.push(i as f32);
         source_vec_b.push(i as f32 * 2.);
         ans.push(i as f32);
     }
-    let source_array_a = Array::from_vec(
-        source_vec_a, 
-        &vec![10, 100], 
-        &status).unwrap();
+    let source_array_a = Array::from_vec(source_vec_a, &vec![10, 10], &status).unwrap();
 
-    let source_array_b = Array::from_vec(
-        source_vec_b, 
-        &vec![10, 100], 
-        &status).unwrap();
+    let source_array_b = Array::from_vec(source_vec_b, &vec![10, 10], &status).unwrap();
 
     let res = source_array_b - source_array_a;
 
