51ec82d8ff0e9c0623da4a3f4f84cd62ab1b693c
bokutotu <mushin.hudoushin@gmail.com>
2021-09-01 00:03:35 +0900

    update function not to get cursstate in args

--- src/cublas/level1.rs
+++ src/cublas/level1.rs
@@ -1,11 +1,10 @@
 use crate::array::Array;
-use crate::CursState;
 use cublas_sys::*;
 
-pub fn saxpy<'a>(handle: &'a CursState, alpha: f32, x: &Array<'a, f32>, y: &Array<'a, f32>) {
+pub fn saxpy<'a>(alpha: f32, x: &Array<'a, f32>, y: &Array<'a, f32>) {
     let result = unsafe {
         cublasSaxpy_v2(
-            handle.cublas_handle,
+            x.state.cublas_handle,
             x.dim.size() as i32,
             &alpha as *const f32,
             x.data_ptr as *const f32,
@@ -19,10 +18,10 @@ pub fn saxpy<'a>(handle: &'a CursState, alpha: f32, x: &Array<'a, f32>, y: &Arra
     }
 }
 
-pub fn daxpy<'a>(handle: &'a CursState, alpha: f64, x: &Array<'a, f64>, y: &Array<'a, f64>) {
+pub fn daxpy<'a>(alpha: f64, x: &Array<'a, f64>, y: &Array<'a, f64>) {
     let result = unsafe {
         cublasDaxpy_v2(
-            handle.cublas_handle,
+            x.state.cublas_handle,
             x.dim.size() as i32,
             &alpha as *const f64,
             x.data_ptr as *const f64,
