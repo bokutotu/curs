04a7dab499a9aaab4206557a41de99e3b4476f1e
bokutotu <mushin.hudoushin@gmail.com>
2021-09-14 00:40:16 +0900

    update test

--- tests/test_compare.rs
+++ tests/test_compare.rs
@@ -1,5 +1,6 @@
-use curs::{CursState};
+use curs::array::compare::Equal;
 use curs::array::Array;
+use curs::CursState;
 
 #[test]
 fn test_eq() {
@@ -8,6 +9,5 @@ fn test_eq() {
     let device_array_a: Array<f32> = Array::ones(&vec![256usize], &status).unwrap();
     let device_array_b: Array<f32> = Array::ones(&vec![256usize], &status).unwrap();
 
-
-    let _res_array = device_array_a.eq(&device_array_b).unwrap();
+    let _res_array = device_array_a.eq(&device_array_b);
 }
