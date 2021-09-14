c17d8248f7f5ec09d113dc19a8b245dfc81b1f78
bokutotu <mushin.hudoushin@gmail.com>
2021-09-14 15:30:24 +0900

    fmt

--- tests/test_alloc.rs
+++ tests/test_alloc.rs
@@ -3,9 +3,8 @@ use curs::array::Array;
 
 #[test]
 fn test_alloc_array_on_cuda() {
-
     let state = curs::CursState::new(0usize);
-    let array = Array::<f32>::zeros(&vec![10,10], &state).unwrap();
+    let array = Array::<f32>::zeros(&vec![10, 10], &state).unwrap();
     array.fill(10f32).unwrap();
     let _host_array = array.as_vec().unwrap();
 }
